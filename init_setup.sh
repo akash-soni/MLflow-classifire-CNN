83fa5919138bf80bc9385309cc4839385cfaf7c5
akash-soni <akash.200287@gmail.com>
2022-09-20 11:48:56 +0530

    model creation done

--- init_setup.sh
+++ init_setup.sh
@@ -1,7 +1,6 @@
 echo [$(date)]: "START"
 export _VERSION_=3.7
 echo [$(date)]: "creating environment with python ${_VERSION_}"
-eval $(conda shell.bash hook)
 conda create --prefix ./env python=${_VERSION_} -y
 echo [$(date)]: "activate environment"
 source activate ./env
